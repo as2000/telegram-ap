d33f212f3b26f418b32f1e7ca8218a2534d2b615
aiden <aidenscott12@googlemail.com>
2016-04-24 00:50:23 +0100

    now able to parse weird time string

--- src/telegram-bot.rb
+++ src/telegram-bot.rb
@@ -6,11 +6,21 @@ token = '211537550:AAGSZIPYI1SaBDYtssv8dNGC81rD-rdJQTU'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     stats = message.text.split("\n")
-    stats.each do |stat|
-      data = stat.split(":")[1]
-      puts data
-      #data = stat.split(":")[1].strip
-      
+    if stats[0] == "aiden" then
+      stats.each do |stat|
+        data = stat.split(":",2)
+
+        case data[0]
+        when "Time"
+          time = DateTime.strptime(data[1], "%R:%S, %a,%m/ %d/%Y")
+          puts "time is #{time}"
+        when "Weight"
+          puts "fatty"
+        end
+        #data = stat.split(":")[1].strip
+        
+      end
     end
   end
 end
+
