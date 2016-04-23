61e7e95e3edee138c2b2829719e527e41bf59d2b
aiden <aidenscott12@googlemail.com>
2016-04-24 00:10:19 +0100

    begin work on message parser

--- src/telegram-bot.rb
+++ src/telegram-bot.rb
@@ -1,17 +1,16 @@
-
 require 'telegram/bot'
 
 token = '211537550:AAGSZIPYI1SaBDYtssv8dNGC81rD-rdJQTU'
 
-Telegram::Bot::Client.run(token, logger: Logger.new($stdout)) do |bot|
+#Telegram::Bot::Client.run(token, logger: Logger.new($stdout)) do |bot|
+Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-   	else
-   		bot.logger.info message.text
+    stats = message.text.split("\n")
+    stats.each do |stat|
+      data = stat.split(":")[1]
+      puts data
+      #data = stat.split(":")[1].strip
+      
     end
   end
 end
