5ec5017d606384cb589e8bd95d38ee37dba91f11
aiden <aidenscott12@googlemail.com>
2016-04-24 01:16:56 +0100

    parses data from iwellness in to json format to be accepted by weightapp

--- src/telegram-bot.rb
+++ src/telegram-bot.rb
@@ -1,6 +1,9 @@
 require 'telegram/bot'
+require 'json'
+
 
 token = '211537550:AAGSZIPYI1SaBDYtssv8dNGC81rD-rdJQTU'
+statHash ={:aiden => {}}
 
 #Telegram::Bot::Client.run(token, logger: Logger.new($stdout)) do |bot|
 Telegram::Bot::Client.run(token) do |bot|
@@ -12,14 +15,29 @@ Telegram::Bot::Client.run(token) do |bot|
 
         case data[0]
         when "Time"
-          time = DateTime.strptime(data[1], "%R:%S, %a,%m/ %d/%Y")
-          puts "time is #{time}"
+          date = DateTime.strptime(data[1], "%R:%S, %a,%m/ %d/%Y")
+          statHash[:aiden][:date] = date.to_s
         when "Weight"
-          puts "fatty"
+          statHash[:aiden][:body_weight] = data[1].tr("kg", '')
+        when "Body Water"
+          statHash[:aiden][:body_water] = data[1].tr("%", '')
+        when "Body Fat"
+          statHash[:aiden][:body_fat] = data[1].tr("%", '')
+        when "Bone"
+          statHash[:aiden][:bone_weight] = data[1].tr("%", '')
+        when "BMI"
+          statHash[:aiden][:bmi] = data[1].tr("%", '')
+        when "Visceral Fat"
+          statHash[:aiden][:visceral_fat] = data[1].tr("%", '')
+        when "BMR"
+          statHash[:aiden][:bmr] = data[1].tr(" kcal", '')
+        when "Muscle Mass"
+          statHash[:aiden][:muscle_mass] = data[1].tr("%", '')
         end
-        #data = stat.split(":")[1].strip
+        
         
       end
+      puts JSON.generate(statHash)
     end
   end
 end
